6f7c17bd9e6b29234d1f31669dd50a472dd31e73
orandorisk <danjentaifib@students.amikom.ac.id>
2021-02-27 15:34:21 +0700

    menambahkan

---
+++ 1.JAVASCRIPT BASIC/8.POP OP BOX JS/script.js
@@ -0,0 +1,22 @@
+// PENGULANGAN//
+// alert('selamat datang');
+// var lagi= true;
+
+// while(lagi === true){
+//     var nama=prompt('Siapa namamu');
+//     alert('nama saya '+nama);
+
+//     lagi=confirm('coba lagi?');
+// }
+// alert('terima kasih');
+
+//PERCABANGAN//
+// var tes = confirm('apa kamu yakin?');
+// if (tes === true){
+// alert('user menekan tombol ok');}
+// else
+// {alert('user menekan tombol cancel');}
+
+//PENGGUNAAN PROMPT//
+var nama = prompt('masukan nama:');
+alert(nama);
